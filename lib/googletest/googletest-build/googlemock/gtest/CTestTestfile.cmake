0fa4546674cf23893c178fe5292a5ae57e037ad2
Boyang Wang <wangjes1202745@gastonchristian.org>
2019-12-12 21:33:18 -0800

    fixed

--- lib/googletest/googletest-build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/boyang18/Homework_7/lib/googletest/googletest-src/googletest
-# Build directory: /home/boyang18/Homework_7/lib/googletest/googletest-build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
